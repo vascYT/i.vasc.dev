171097ae63ca26f160535354443ea91d2c9d5cc4
vascYT <nicovanaschaufler@gmail.com>
2022-05-07 17:18:31 +0200

    remove: loading error msg

--- src/pages/[fileName].tsx
+++ src/pages/[fileName].tsx
@@ -1,13 +1,4 @@
-import {
-  Box,
-  Center,
-  Heading,
-  HStack,
-  Icon,
-  Image,
-  Text,
-  useBoolean,
-} from "@chakra-ui/react";
+import { Box, Center, HStack, Icon, Image, Text } from "@chakra-ui/react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { BiImage } from "react-icons/bi";
@@ -15,7 +6,6 @@ import { BiImage } from "react-icons/bi";
 export default function FileViewer() {
   const router = useRouter();
   const { fileName } = router.query;
-  const [failed, setFailed] = useBoolean();
 
   return (
     <>
@@ -25,27 +15,22 @@ export default function FileViewer() {
           property="og:image"
           content={`${process.env.NEXT_PUBLIC_S3_PUBLIC_URL}/${fileName}`}
         />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Center h="100vh" w="full" bgColor="gray.900">
         <Box boxShadow="2xl" borderRadius={10} bgColor="gray.800" padding={5}>
-          {!failed ? (
-            <>
-              <Image
-                src={`${process.env.NEXT_PUBLIC_S3_PUBLIC_URL}/${fileName}`}
-                onError={setFailed.on}
-                borderRadius={5}
-                alt="Uploaded image"
-              />
-              <HStack pt={2} justifyContent="center">
-                <Icon as={BiImage} color="white" w={7} h={7} />
-                <Text color="white" fontSize="2xl">
-                  {fileName}
-                </Text>
-              </HStack>
-            </>
-          ) : (
-            <Heading color="white">Failed to load image</Heading>
-          )}
+          <Image
+            src={`${process.env.NEXT_PUBLIC_S3_PUBLIC_URL}/${fileName}`}
+            borderRadius={5}
+            alt="Uploaded image"
+            w="full"
+          />
+          <HStack pt={2} justifyContent="center">
+            <Icon as={BiImage} color="white" w={7} h={7} />
+            <Text color="white" fontSize="2xl">
+              {fileName}
+            </Text>
+          </HStack>
         </Box>
       </Center>
     </>
